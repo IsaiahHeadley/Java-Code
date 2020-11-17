872511b6f762a9ea9d584b6f4e853ce9f20ea919
Isaiah Headley <36669288+IsaiahHeadley@users.noreply.github.com>
2020-11-17 15:55:09 -0500

    Update ifelseif.java

--- ifelseif.java
+++ ifelseif.java
@@ -1,18 +1,23 @@
-import java.util.Scanner;
+iimport java.util.Scanner;
 
 public class ifelseif{
 
 	
     public static void main(String[] args) {
                
+ Scanner age = new Scanner(System.in);  // Create a Scanner object
 
-	int age=15;
+    System.out.println("Enter your age");
 
-	if(age<=0){
+ int userAge = age.nextInt();  // Read user input
+ 
+	//int age=170;
+
+	if(userAge <= 0){
 	 System.out.println("Error");
-}else if(age<=16){
-	 System.out.println("Too Young");
-}else if(age<100){
+}else if(userAge <= 18){
+	 System.out.println("You are too young to enter");
+}else if(userAge > 18){
 	 System.out.println("Welcome!");
 }else {
 	 System.out.println("Really?");
