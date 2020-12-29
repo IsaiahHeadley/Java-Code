9ee1003a9c255f4ff2b8d375b60c0a4cbfe89f15
Isaiah Headley <36669288+IsaiahHeadley@users.noreply.github.com>
2020-12-29 18:42:46 -0500

    Add files via upload

---
+++ Member.java
@@ -0,0 +1,53 @@
+import java.io.*;
+
+/* This is an object class named Member that exhibits fields, a constructor w parameters, and getter setter methods 
+*/
+
+public class Member {
+  
+    private char memberType;
+    private int memberId;
+    private String name;
+    private double fees;
+  
+  public Member(char pmemberType, int pmemberId, String pName, double pFees){
+
+
+ public void setMemberType(char pMemberType)
+ {
+   memberType = pMemberType;
+ }
+ public char getMemberType()
+ {
+   return memberType;
+ }
+ public void setMemberId(int pMemberId)
+ {
+   memberId = pMemberId;
+ }
+ public int getMemberId()
+ {
+   return memberId;
+ }
+  public void setname(String pName)
+ {
+   name = pName;
+ }
+ public String getname()
+ {
+   return name;
+ }
+  public void setfees(double pFees)
+ {
+   fees = pFees;
+ }
+ public char getfees()
+ {
+   return fees;
+ }
+ @Override
+ public String toString(){
+   return memberType + ", " + memberId + ", " + name + ", " + fees; 
+ }
+}
+  }
